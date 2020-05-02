a3d0677510d01f6ae7085b82bcf45685e842ddd2
zero <13260648390@163.com>
2020-05-02 14:41:37 +0800

    fix scss

--- web/src/main.js
+++ web/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './style.scss'
+import './assets/scss/style.scss'
 
 Vue.config.productionTip = false
 
---
+++ web/src/views/Main.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="topbar bg-black py-2 w-100">
+      asd
+    </div>
+    <div class="bg-primary">
+      <div class="nav">
+        <div class="nav-item">
+          <router-link tag="div" to="/">首页</router-link>
+        </div>
+        <div class="nav-item">
+          <router-link tag="div" to="/">攻略中心</router-link>
+        </div>
+        <div class="nav-item">
+          <router-link tag="div" to="/">赛事中心</router-link>
+        </div>
+      </div>
+    </div>
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+</script>
